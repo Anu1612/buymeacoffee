bfb3351dcaa3561d130da29f0844b9e27f037ecc
Anurag <myanuragojha@gmail.com>
2019-08-20 13:04:56 +0530

    Updates

---
+++ precache-manifest.33c9996ebbabd437f509b2690cc48144.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db3c25b89bdfc436a8a3558382bc659f",
+    "url": "/buymeacoffee/index.html"
+  },
+  {
+    "revision": "541c1bc389293a865b46",
+    "url": "/buymeacoffee/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "6602ccdd001cdc04037c",
+    "url": "/buymeacoffee/static/css/main.0420495a.chunk.css"
+  },
+  {
+    "revision": "541c1bc389293a865b46",
+    "url": "/buymeacoffee/static/js/2.a662da01.chunk.js"
+  },
+  {
+    "revision": "6602ccdd001cdc04037c",
+    "url": "/buymeacoffee/static/js/main.de7f187d.chunk.js"
+  },
+  {
+    "revision": "47cfb50b539d300d7911",
+    "url": "/buymeacoffee/static/js/runtime~main.afb1d10b.js"
+  },
+  {
+    "revision": "d11960c98b57d285928f213af78a33aa",
+    "url": "/buymeacoffee/static/media/PinClipart.com_microphone-images-clip-art_201802.d11960c9.png"
+  },
+  {
+    "revision": "24a664749cd5c1f0fda0d748066923fd",
+    "url": "/buymeacoffee/static/media/joanna-kosinska-1_CMoFsPfso-unsplash.24a66474.jpg"
+  },
+  {
+    "revision": "05b981f48d67d250de9a155a69096366",
+    "url": "/buymeacoffee/static/media/kawaii-coffee-mug-cartoon-vector-23675241.05b981f4.jpg"
+  }
+]);
